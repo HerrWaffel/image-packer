5f072fd167baa2600db00d9a08fdf0365b8e3319
Quint Vrolijk <herrwaffel@gmail.com>
2025-11-26 16:43:50 +0100

    doc: updated to version 1.0.6

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Image Packer",
     "description": "Combine multiple images into a single image using different packing algorithms",
     "author": "Quint Vrolijk",
-    "version": (1, 0, 5),
+    "version": (1, 0, 6),
     "blender": (3, 0, 0),
     "location": "Image Editor",
     "category": "Paint"
